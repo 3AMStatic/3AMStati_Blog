77cc48b5b64ba8ccdd56feaa707d5010b35c6600
3AMStatic <work@innatu.cn>
2026-09-03 02:03:01 +0800

    Auto sync source code for Vercel 🚀

--- siteConfig.ts
+++ siteConfig.ts
@@ -2,12 +2,12 @@
 
 export const siteConfig = {
   // 1. 网站标题与博主信息
-  title: "XingHuiSama の 宝藏之地",
+  title: "3AMStatic の 宝藏之地",
   faviconUrl: "https://bu.dusays.com/2026/03/24/69c1e38ac1846.jpg",
-  authorName: "XingHuiSama",
+  authorName: "3AMStatic",
   bio: "在代码、学术与分子动力学模拟间穿梭的普通人。近期正埋头于 GROMACS 模拟研究与神经网络计算。",
 
-  navTitle: "XingHuiSama",
+  navTitle: "3AMStatic",
 
   // 👇 【新增】导航栏中间的那个后缀/分隔符（默认是 の）
   navSuffix: "の",
@@ -31,10 +31,10 @@ export const siteConfig = {
   photoWallImage: "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
   cloudMusicIds: ["1809646618", "3361076230", "1859390262"],
   social: {
-    github: "",
-    gitee: "",
-    google: "",
-    email: "",
+    github: "https://github.com/heiehiehi",
+    gitee: "https://github.com/heiehiehi",
+    google: "mailto:bilibiliwuwuwu@gmail.com",
+    email: "work@innatu.cn",
     qq: "1124533793",
     wechat: "XingHuisama",
   },
@@ -54,11 +54,11 @@ export const siteConfig = {
     owner: "",
     admin: [""],
   },
-  buildDate: "2026-03-23T00:00:00", // 建站日期
+  buildDate: "2026-09-03T00:00:00", // 建站日期
   footerBadges: [{"name": "Next.js 15", "color": "text-sky-500", "svg": "<path d=\"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z\"/>"}, {"name": "React 19", "color": "text-cyan-400", "svg": "<path d=\"M12 22.6l-9.8-5.6V5.6L12 0l9.8 5.6v11.4l-9.8 5.6zm-8.2-6.5l8.2 4.7 8.2-4.7V7.5L12 2.8 3.8 7.5v8.6z\"/>"}, {"name": "Tailwind 4", "color": "text-teal-400", "svg": "<path d=\"M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624C13.666,10.618,15.027,12,18.001,12 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624c1.177,1.194,2.538,2.576,5.512,2.576 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C10.337,13.382,8.976,12,6.001,12z\"/>"}],
   icpConfig: {
-    name: "萌ICP备 20260240号",
-    link: "https://icp.gov.moe/?keyword=20260240",
+    name: "粤ICP备2026037708号",
+    link: "https://www.beian.gov.cn",
   },
   geminiConfig: {
     modelId: "gemini-2.5-flash-lite",
